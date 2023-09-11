174ea51909431b7c613539535bda1fcf6987efa0
rosom <yuoran371@gmail.com>
2023-09-11 19:20:56 +0300

    Add finished project

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,42 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+//    	ArrayDeque<Integer> que = new ArrayDeque<Integer>();
+//    	while (!firstQueue.poll().equals("null")&&!secondQueue.poll().equals("null")) {
+//    	if (firstQueue.poll().equals("null")&&secondQueue.poll().equals("null")) {
+//    		return que;
+//    	} else {
+//    	que.add(firstQueue.poll());
+//    	que.add(secondQueue.poll());
+//    	firstQueue.add(que.poll());
+//    	secondQueue.add(que.poll());
+//    	que.add(firstQueue.poll());
+//    	que.add(secondQueue.poll());
+//    	}
+//    	}
+//		return que;
+    	
+    	ArrayDeque<Integer> que = new ArrayDeque<Integer>();
+    	que.add(firstQueue.poll());
+    	que.add(firstQueue.poll());
+    	que.add(secondQueue.poll());
+    	que.add(secondQueue.poll());
+    	while (firstQueue.peek()!=null&&secondQueue.peek()!=null) {
+	    	firstQueue.add(que.pollLast());
+	    	que.add(firstQueue.poll());
+	    	que.add(firstQueue.poll());
+	    	secondQueue.add(que.pollLast());
+	    	que.add(secondQueue.poll());
+	    	que.add(secondQueue.poll());
+
+    	}
+		return que;
+    	
     }
 }
