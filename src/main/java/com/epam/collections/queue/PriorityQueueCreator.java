bcdeeaf5decfbba5e9ac17aea044a71717c2889a
rosom <yuoran371@gmail.com>
2023-09-11 17:53:17 +0300

    Add finished project

--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,16 @@
 package com.epam.collections.queue;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+    	
+    	PriorityQueue<String> pq = new PriorityQueue<String>(Comparator.reverseOrder());
+    	pq.addAll(firstList);
+    	pq.addAll(secondList);
+		return pq;
     }
 }
+
